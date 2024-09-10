c6871854f3251d74feebdbe992281b238f474f1f
awrunpatel <arunp3989@gmail.com>
2024-09-10 14:24:14 +0545

    delete confirmation page added

--- ismt/settings.py
+++ ismt/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = "ismt.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ismt', 
-#         'USER': 'ismt',
-#         'PASSWORD': 'ISMTdatabase', 
-#         'HOST': '93.127.194.205',
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ismt', 
+        'USER': 'ismt',
+        'PASSWORD': 'ISMTdatabase', 
+        'HOST': '93.127.194.205',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
