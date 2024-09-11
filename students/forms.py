4dfbbb2ac7ed0835643a534baf40602aad269102
awrunpatel <arunp3989@gmail.com>
2024-09-11 12:56:25 +0545

    file manager added

--- students/forms.py
+++ students/forms.py
@@ -1,21 +1,26 @@
 from django import forms
-from .models import Admission, EducationalHistory
+from .models import *
 
-class AdmissionForm(forms.ModelForm):
+
+class StudentForm(forms.ModelForm):
     class Meta:
-        model = Admission
-        fields = [
-            'first_name', 'middle_name', 'last_name', 'gender', 
-            'date_of_birth_in_bs', 'date_of_birth_in_ad', 'address', 
-            'province', 'country', 'postcode', 'mobile_number', 'email',
-            'date_of_admission', 'commencing_term', 'shift', 
-            'admission_officer', 'pp_image', 'citizenship_number', 
-            'campus', 'department', 'program', 'modules'
-        ]
+        model = Students
+        fields = ['subject', 'institute', 'grade', 'passed_year', 'country_studied','date_of_admission', 'commencing_term', 'shift','admission_officer', 'campus', 'department', 'program', 'modules', 'gender', 'date_of_birth_in_bs', 'date_of_birth_in_ad', 'address', 'province', 'country', 'postcode', 'citizenship_number'
+            ]
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
-            'middle_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Middle Name'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter subject'}),
+            'institute': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter institute'}),
+            'grade': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter grade'}),
+            'passed_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter passed year'}),
+            'country_studied': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter country studied'}),
+            'campus': forms.Select(attrs={'class': 'form-control'}),
+            'department': forms.Select(attrs={'class': 'form-control'}),
+            'program': forms.Select(attrs={'class': 'form-control'}),
+            'modules': forms.Select(attrs={'class': 'form-control'}),
+            'date_of_admission': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'commencing_term': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Commencing Term'}),
+            'shift': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Shift'}),
+            'admission_officer': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Admission Officer'}),
             'gender': forms.Select(attrs={'class': 'form-control'}),
             'date_of_birth_in_bs': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'BS Date'}),
             'date_of_birth_in_ad': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'AD Date', 'type': 'date'}),
@@ -23,29 +28,5 @@ class AdmissionForm(forms.ModelForm):
             'province': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Province'}),
             'country': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}),
             'postcode': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Postcode'}),
-            'mobile_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Mobile Number'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-            'date_of_admission': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'commencing_term': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Commencing Term'}),
-            'shift': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Shift'}),
-            'admission_officer': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Admission Officer'}),
-            'pp_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'citizenship_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Citizenship Number'}),
-            'campus': forms.Select(attrs={'class': 'form-control'}),
-            'department': forms.Select(attrs={'class': 'form-control'}),
-            'program': forms.Select(attrs={'class': 'form-control'}),
-            'modules': forms.Select(attrs={'class': 'form-control'}),
-        }
-
-
-class EducationalHistoryForm(forms.ModelForm):
-    class Meta:
-        model = EducationalHistory
-        fields = ['subject', 'institute', 'grade', 'passed_year', 'country_studied']
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter subject'}),
-            'institute': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter institute'}),
-            'grade': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter grade'}),
-            'passed_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter passed year'}),
-            'country_studied': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter country studied'}),
         }
--- students/models.py
+++ students/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 from dashboard.models import Campus, Department, Program, Modules
+from userauth.models import *
 
-
-class Admission(models.Model):
-    student_id = models.AutoField(primary_key=True)  # Auto-incrementing ID
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100)
-    
-    date_of_admission = models.DateField()
-    commencing_term = models.CharField(max_length=100)
-    shift = models.CharField(max_length=50)
-    admission_officer = models.CharField(max_length=100)
-    pp_image = models.ImageField(upload_to='images/', blank=True, null=True)
-    citizenship_number = models.CharField(max_length=20)
-    
+class Students(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
@@ -24,30 +13,29 @@ class Admission(models.Model):
     
     date_of_birth_in_bs = models.CharField(max_length=20)
     date_of_birth_in_ad = models.DateField()
+    citizenship_number = models.CharField(max_length=20)
     
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True, null=True)  # Set as nullable
     province = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     postcode = models.CharField(max_length=10)
-    mobile_number = models.CharField(max_length=15)
-    email = models.EmailField()
+
+    subject = models.CharField(max_length=100)
+    institute = models.CharField(max_length=200)
+    grade = models.CharField(max_length=10)
+    passed_year = models.IntegerField()
+    country_studied = models.CharField(max_length=100)
+
+    date_of_admission = models.DateField()
+    shift = models.CharField(max_length=50)
+    admission_officer = models.CharField(max_length=100)
+    commencing_term = models.CharField(max_length=100)
 
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
     modules = models.ForeignKey(Modules, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} (ID: {self.student_id})"
-    
-
-class EducationalHistory(models.Model):
-    student = models.ForeignKey(Admission, on_delete=models.CASCADE, related_name='educational_history')
-    subject = models.CharField(max_length=100)
-    institute = models.CharField(max_length=200)
-    grade = models.CharField(max_length=10)
-    passed_year = models.IntegerField()
-    country_studied = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.subject} - {self.institute}"
