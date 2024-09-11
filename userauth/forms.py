a9d0977d45c882067e334b2ccdbed47a791a1fce
Prakash Luitel <prakashluitel991@gmail.com>
2024-09-11 13:47:41 +0545

    Added models for education history and language test

--- userauth/forms.py
+++ userauth/forms.py
@@ -5,12 +5,12 @@ class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'test@example.com'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder' : '********'}))
 
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = [
-            'first_name', 'middle_name', 'last_name', 'mobile_number', 'profile_image','email'
-        ]
+        fields = ['email', 'username', 'first_name', 'middle_name', 'last_name', 'profile_image', 'contact_number']
+
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
             'middle_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Middle Name'}),
--- userauth/models.py
+++ userauth/models.py
@@ -1,14 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ismt.settings import STATIC_URL
+from django.contrib.auth.models import BaseUserManager
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser ):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, default="")
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    profile_image = models.ImageField(blank=True, null=True)
+    contact_number = models.CharField(max_length=100)
     
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
 
     def save(self, *args, **kwargs):
         if not self.username and self.email:
@@ -21,18 +45,31 @@ class User(AbstractUser):
             self.username = unique_username
         super().save(*args, **kwargs)
     
-class UserDetail(models.Model):
-    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserDetail')
 
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100)
-    profile_image = models.ImageField(blank=True, null=True)
-    contact_number = models.CharField(max_length=100)
+
+class EducationHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    degree_name = models.CharField(max_length=100)
+    institution_name = models.CharField(max_length=100)
+    graduation_year = models.IntegerField()
+    major_subject = models.CharField(max_length=100)
+    files = models.FileField(upload_to='pdf/', blank=True, null=True)
 
     class Meta:
         abstract = True
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} (ID: {self.id})"
+
+class EnglishTest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ielts = models.CharField(max_length= 5)
+    toefl = models.CharField(max_length= 5)
+    pte = models.CharField(max_length=5)
+    cambridge_test = models.CharField(max_length=5)
+    other = models.CharField(max_length= 5)
+    files = models.FileField(upload_to='pdf/', blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    
 
