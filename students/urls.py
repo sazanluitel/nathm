de0eaf8f07e1c8bc1d6073f53b1beb99c7c8243a
awrunpatel <arunp3989@gmail.com>
2024-09-13 23:19:13 +0545

    error fixed

--- students/urls.py
+++ students/urls.py
@@ -6,6 +6,6 @@ app_name = "students"
 urlpatterns = [
     path('student/add/', StudentView.as_view(), name = 'student'),
     path('student/edit/<int:id>', StudentEditView.as_view(), name = 'studentedit'),
-    path('student/list', StudentList.as_view(), name = 'studentlist'),
+    path('student/list/', StudentList.as_view(), name = 'studentlist'),
     path('student/ajax', StudentAjax.as_view(), name = 'studentajax'),
 ]
