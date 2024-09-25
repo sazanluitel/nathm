44318dde70caa1200c15a049cd33b71d2b4070cf
Prakash Luitel <prakashluitel991@gmail.com>
2024-09-25 15:36:58 +0545

    Added the books and library sections

---
+++ library/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Book
+from .models import Library
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'author', 'publication_year', 'publication_house', 'program']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'book_name', 'placeholder': 'Book Name'}),
+            'author': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'book_author', 'placeholder': 'Author Name'}),
+            'publication_year': forms.NumberInput(
+                attrs={'class': 'form-control', 'id': 'publication_year', 'placeholder': 'Publication Year'}),
+            'publication_house': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'publication_house', 'placeholder': 'Publication House'}),
+            'program': forms.Select(
+                attrs={'class': 'form-control', 'id': 'program'}),
+        }
+
+
+class LibraryForm(forms.ModelForm):
+    class Meta:
+        model = Library
+        fields = ['book', 'borrowed_by', 'return_date']
+        widgets = {
+            'book': forms.Select(
+                attrs={'class': 'form-control', 'id': 'library_book'}),
+            'borrowed_by': forms.Select(
+                attrs={'class': 'form-control', 'id': 'library_borrowed_by'}),
+            'return_date': forms.DateInput(
+                attrs={'class': 'form-control', 'id': 'return_date', 'placeholder': 'Return Date', 'type': 'date'}),
+        }
---
+++ library/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from dashboard.models import Program
+from students.models import Student
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    publication_house = models.CharField(max_length=200)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
+
+class Library(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_by = models.ForeignKey(Student, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.book} - borrowed by {self.borrowed_by}"
