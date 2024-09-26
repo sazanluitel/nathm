00302aae9ea9c43a073a3ebec47f057d623d74a0
Prakash Luitel <prakashluitel991@gmail.com>
2024-09-26 11:09:26 +0545

    Made the certificate app

---
+++ certificate/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class RequestCertificate(models.Model):
+    CERTIFICATE_TYPE = [
+        ('Character', 'Characher Certificate'),
+        ('Training', 'Training Certificate'),
+        ('Academic', 'Academic Certificate'),
+    ]
+    student = models.ForeignKey('students.Student', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    certificate_type = models.CharField(max_length=100, choices=CERTIFICATE_TYPE)
