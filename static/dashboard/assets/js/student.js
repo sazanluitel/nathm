a14742fc8fd58b9f84bb3198dcd7697658507a92
awrunpatel <arunp3989@gmail.com>
2024-09-28 22:41:33 +0545

    update

--- static/dashboard/assets/js/student.js
+++ static/dashboard/assets/js/student.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-    // Total form count variables for each formset
     let educationFormCount = $('#id_educational_history-TOTAL_FORMS').val();
     let workFormCount = $('#id_employment_history-TOTAL_FORMS').val();
     let testFormCount = $('#id_english_test-TOTAL_FORMS').val();
 
-    // Function to dynamically update form indices
     function updateFormIndex(selector, prefix, formCount) {
         $(selector).each(function (index) {
             $(this).find('input, select, textarea').each(function () {
@@ -15,17 +13,15 @@ $(document).ready(function () {
         });
     }
 
-    // Add new Educational History form
     $('.add_more_education').click(function () {
         let newForm = $('#form-templates .educational_history_form').clone();
-        newForm.html(newForm.html().replace(/__prefix__/g, educationFormCount)); // Update prefix
+        newForm.html(newForm.html().replace(/__prefix__/g, educationFormCount));
         $('#educational-history-forms').append(newForm.html());
         educationFormCount++;
         $('#id_educational_history-TOTAL_FORMS').val(educationFormCount);
         updateFormIndex('.education-form', 'educational_history', educationFormCount);
     });
 
-    // Remove Educational History form
     $(document).on('click', '.remove-education-form', function () {
         $(this).closest('.education-form').remove();
         educationFormCount--;
@@ -33,17 +29,15 @@ $(document).ready(function () {
         $('#id_educational_history-TOTAL_FORMS').val(educationFormCount);
     });
 
-    // Add new Work Experience form
     $('.add-more').click(function () {
         let newForm = $('#form-templates .employment_history_form').clone();
-        newForm.html(newForm.html().replace(/__prefix__/g, workFormCount)); // Update prefix
+        newForm.html(newForm.html().replace(/__prefix__/g, workFormCount));
         $('#work-experience-forms').append(newForm.html());
         workFormCount++;
         $('#id_employment_history-TOTAL_FORMS').val(workFormCount);
         updateFormIndex('.work-form', 'employment_history', workFormCount);
     });
 
-    // Remove Work Experience form
     $(document).on('click', '.remove-work-form', function () {
         $(this).closest('.work-form').remove();
         workFormCount--;
@@ -51,21 +45,40 @@ $(document).ready(function () {
         $('#id_employment_history-TOTAL_FORMS').val(workFormCount);
     });
 
-    // Add new English Test form
     $('.add-more-test').click(function () {
         let newForm = $('#form-templates .english_test_form').clone();
-        newForm.html(newForm.html().replace(/__prefix__/g, testFormCount)); // Update prefix
+        newForm.html(newForm.html().replace(/__prefix__/g, testFormCount));
         $('#form-container').append(newForm.html());
         testFormCount++;
         $('#id_english_test-TOTAL_FORMS').val(testFormCount);
         updateFormIndex('.test-form', 'english_test', testFormCount);
     });
 
-    // Remove English Test form
     $(document).on('click', '.remove-test-form', function () {
         $(this).closest('.test-form').remove();
         testFormCount--;
         updateFormIndex('.test-form', 'english_test', testFormCount);
         $('#id_english_test-TOTAL_FORMS').val(testFormCount);
+    });
+
+    $('#libraryForm').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '{% url "students:studentstatus" %}',
+            data: $(this).serialize(),
+            success: function (response) {
+                $('#libraryModal').modal('hide');
+                alert('Book requested successfully!');
+            },
+            error: function (xhr) {
+                var errors = xhr.responseJSON.errors || {};
+                var errorMessage = 'Please fix the following errors:\n';
+                $.each(errors, function (field, messages) {
+                    errorMessage += field + ': ' + messages.join(', ') + '\n';
+                });
+                alert(errorMessage);
+            }
+        });
     });
 });
