8f06a97e05c9e102637dc5136e7765f0137e9ccc
Suresh Chand <scthakuri12a@gmail.com>
2024-09-29 11:15:36 +0545

    update

--- routine/admin_urls.py
+++ routine/admin_urls.py
@@ -4,7 +4,7 @@ from routine.views import *
 app_name = "ismt"
 
 urlpatterns = [
+    path("routine/class/", ClassRoutineView.as_view(), name="class_routines"),
     path("routine/<section_id>/", RoutineView.as_view(), name="routine"),
-    path("routine/<section_id>/events/", RoutineEventsView.as_view(), name="routine_events"),
+    path("routine/<section_id>/events/", RoutineEventsView.as_view(), name="routine_events")
 ]
-
