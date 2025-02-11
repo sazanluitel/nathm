8127e4b9f9cfd326fb7b2ea48064873ba44c0106
awrunpatel <arunp3989@gmail.com>
2025-02-11 13:21:04 +0545

    multi delete

--- mail/modules/welcome.py
+++ mail/modules/welcome.py
@@ -20,8 +20,8 @@ class WelcomeMessage(EmailHelper):
 
         token = default_token_generator.make_token(self.user)
         uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        reset_link = f"http://127.0.0.1:8000/reset-password/{uid}/{token}/"
-        # reset_link = f"https://nathm.sunbi.com.np/reset-password/{uid}/{token}/"
+        # reset_link = f"http://127.0.0.1:8000/reset-password/{uid}/{token}/"
+        reset_link = f"https://nathm.sunbi.com.np/reset-password/{uid}/{token}/"
 
         message = self.get_template_content("welcome", {
             "first_name": self.user.first_name,
